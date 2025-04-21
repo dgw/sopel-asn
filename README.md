ddaf5545a3df70a12f6a0c43a98de227e8bebae2
dgw <dgw@technobabbl.es>
2025-04-20 23:09:10 -0500

    Initial proof of concept: IP origin, peer, and ASN detail lookup
    The output format ain't pretty (just the TXT record returned by Cymru
    verbatim for now), but all three lookup types work and input validation
    seems to do OK.

---
+++ README.md
@@ -0,0 +1,15 @@
+# sopel-asn
+
+ASN lookup plugin for Sopel IRC bots
+
+## Installing
+
+Releases are hosted on PyPI, so after installing Sopel, all you need is `pip`:
+
+```shell
+$ pip install sopel-asn
+```
+
+## Using
+
+
---
+++ sopel_asn/plugin.py
@@ -0,0 +1,95 @@
+"""sopel-asn
+
+ASN lookup plugin for Sopel IRC bots
+
+Copyright (c) 2025 dgw, technobabbl.es
+
+Licensed under the Eiffel Forum License 2.
+"""
+from __future__ import annotations
+
+import ipaddress
+import re
+
+import dns.resolver
+
+from sopel import plugin
+
+
+OUTPUT_PREFIX = '[ASN] '
+ENDPOINTS = {
+    'origin': 'origin.asn.cymru.com',
+    'peers': 'peer.asn.cymru.com',
+    'asn': 'asn.cymru.com',
+}
+
+
+@plugin.commands('asn', 'asno', 'asnorigin', 'asnp', 'asnpeers')
+@plugin.example('.asno 198.6.1.65')
+@plugin.output_prefix(OUTPUT_PREFIX)
+@plugin.rate(
+    user=120,
+    message="Please wait {time_left} before attempting another ASN lookup."
+)
+def get_asn_info(bot, trigger):
+    """Look up ASN (Autonomous System Number) and routing information.
+
+    All commands require an IP address or AS number (in `ASxxx` format) as the
+    first & only argument.
+    """
+    if not (ip := trigger.group(3)):
+        bot.reply("Please provide an IP address or ASN.")
+        return plugin.NOLIMIT
+    cmd = trigger.group(1).lower()
+
+    try:
+        ip = ipaddress.ip_address(ip)
+    except (ValueError, ipaddress.AddressValueError):
+        # treat as ASN
+        if not re.match(r'^(AS)?\d+$', ip):
+            bot.reply("Please provide a valid IP address or AS number.")
+            return plugin.NOLIMIT
+        if not ip.startswith('AS'):
+            ip = 'AS' + ip
+        lookup = ip
+    else:
+        lookup = '.'.join([str(x) for x in reversed(ip.packed)])
+
+    if cmd in ('asn'):
+        base = ENDPOINTS['asn']
+    elif cmd in ('asno', 'asnorigin'):
+        base = ENDPOINTS['origin']
+    elif cmd in ('asnp', 'asnpeers'):
+        base = ENDPOINTS['peers']
+    else:
+        bot.reply("Unrecognized command '{}'. How'd you get here?".format(cmd))
+        return plugin.NOLIMIT
+
+    lookup = '.'.join((lookup, base))
+    responses = []
+
+    try:
+        answers = dns.resolver.resolve(lookup, 'TXT')
+    except dns.exception.SyntaxError:
+        bot.reply("That IP address doesn't seem to be valid.")
+        return plugin.NOLIMIT
+    except dns.exception.Timeout:
+        bot.say("Lookup timed out for {}.".format(ip))
+        return plugin.NOLIMIT
+    except dns.resolver.NoNameservers:
+        bot.say("Lookup attempted, but no nameservers were available.")
+        return plugin.NOLIMIT
+    except (dns.resolver.NXDOMAIN, dns.resolver.NoAnswer):
+        bot.say("No records found for {}.".format(ip))
+        return  # do rate-limit, since query succeeded
+
+    if len(answers) > 0:
+        for rdata in answers:
+            responses.append(rdata.to_text())
+    else:
+        bot.say("Did not find any records for {}.".format(ip))
+        return
+
+    # Record types that should be handled one response per line
+    for x in responses:
+        bot.say(x)
